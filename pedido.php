ce0d3e8d823ba12e59313b4a221afe4da0885ee9
Eliezer Lopez <371EZE6.375@GMAIL.COM>
2019-12-12 21:18:39 -0600

    im

--- pedido.php
+++ pedido.php
@@ -29,6 +29,18 @@
                             <p class="text-center">
                               <button class="btn btn-lg btn-raised btn-success btn-block" data-toggle="modal" data-target="#PagoModalTran">Transaccion Bancaria</button>
                             </p>
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                      <input type="hidden" name="cmd" value="_cart">
+                      <input type="hidden" name="upload" value="1">
+                      <input type="hidden" name="business" value="seller@dezignerfotos.com">
+                      <input type="hidden" name="item_name_1" value="Item Name 1">
+                      <input type="hidden" name="amount_1" value="1.00">
+                      <input type="hidden" name="shipping_1" value="1.75">
+                      <input type="hidden" name="item_name_2" value="Item Name 2">
+                      <input type="hidden" name="amount_2" value='200'>
+                      <input type="hidden" name="shipping_2" value="2.50">
+                      <input type="submit" class="btn btn-lg btn-raised btn-success btn-block" value="Paypal">
+              </form>  
                           </div>
                         </div>
                       </div>
